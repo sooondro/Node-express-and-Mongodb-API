4d69564986a7a0102c7ded3aa9b2ece8ed20fafd
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-16 21:43:08 +0100

    Handling requests

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 // Set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+  console.log('GET request');
+  res.send({name: 'Sandro'});
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function(){
   console.log('Now listening for requests');
