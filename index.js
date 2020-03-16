0b91d596bea7e3a7a0da0a4f645fd729e8c03c63
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-16 22:03:24 +0100

    Creating routes

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const express = require('express');
 // Set up express app
 const app = express();
 
-app.get('/api', function(req, res){
-  console.log('GET request');
-  res.send({name: 'Sandro'});
-});
+//Initialize routes
+app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+//Get a list of ninjas from the db
+router.get('/ninjas', function(req, res){
+  res.send({type:'GET'});
+});
+
+//Add a new ninja to the db
+router.post('/ninjas', function(req, res){
+  res.send({type:'POST'});
+});
+
+//Update a ninja in the db
+router.put('/ninjas/:id', function(req, res){
+  res.send({type:'PUT'});
+});
+
+//Delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res){
+  res.send({type:'DELETE'});
+});
+
+module.exports = router;
