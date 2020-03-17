cd64fe404e0a3957ba6b253a9c4ac79d5649e5bd
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-17 22:00:32 +0100

    Handling DELETE requests

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,9 @@ router.put('/ninjas/:id', function(req, res, next){
 
 //Delete a ninja from the db
 router.delete('/ninjas/:id', function(req, res, next){
-  res.send({type:'DELETE'});
+  Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
+    res.send(ninja);
+  });
 });
 
 module.exports = router;
