b1b14a53b0befa21e59d7f98c969947c7393c213
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-17 21:20:09 +0100

    Saving data to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Set up express app
 const app = express();
+
+//Conntect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
 
 //Middleware
 app.use(express.json());
