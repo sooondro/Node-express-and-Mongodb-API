96a358b0c9a47ef977b055aa9f2a11dcd28de04f
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-17 20:56:35 +0100

    Models and schemas - 2

---
+++ models/ninja.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create ninja schema and model
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required']
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  },
+  //Add in geo location
+});
+
+const Ninja = mongoose.model('ninja', NinjaSchema);
+
+module.exports = Ninja;
